cfbe17024ea0168701bc83c7e39a95b4778dd36f
Foodzya <afodzya@gmail.com>
2021-04-14 20:55:52 +0300

    Add project files.

---
+++ ParkingApplication/Car.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingApplication
+{
+    public class Car : IMovable
+    {
+        static List<Car> cars = new List<Car>();
+
+        public string DriveUnit()
+        {
+            //switch (driveUnit)
+            //{
+            //    case "1":
+            //        Console.WriteLine("You drive a front-wheel drive car!");
+            //        break;
+            //    case "2":
+            //        Console.WriteLine("You drive a rear wheel drive car");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Unknown input");
+            //        break;
+            //}
+            return null;
+        }
+
+        public double Speed()
+        {
+            return 0;
+        }
+
+        public static void AddToList(Car car)
+        {
+            cars.Add(car);
+        }
+
+        public string Transmission()
+        {
+            return null;
+        }
+
+        private string Brand { get; set; }
+        private uint ProdYear { get; set; }
+        private double Price { get; set; }
+
+
+        public Car(string brand, uint prodYear, double price)
+        {
+            Brand = brand; ProdYear = prodYear; Price = price;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Brand - {Brand}\nYear of issue - {ProdYear}\nPrice - ${Price}");
+        }
+
+        public static void RentImplementation()
+        {
+            int index = 1;
+            foreach (Car car in cars)
+            {
+                Console.Write($"{index}) ");
+                car.GetInfo();
+                index++;
+            }
+
+            Console.WriteLine("Choose car you want to rent: ");
+            char rentChoice = Console.ReadKey().KeyChar;
+
+            if (char.IsDigit(rentChoice))
+            {
+                int intRentChoice = (int)Char.GetNumericValue(rentChoice); 
+                try
+                {
+                    Console.WriteLine($"You have rented {cars.ElementAt(intRentChoice - 1).Brand}");
+                    cars.RemoveAt(intRentChoice - 1);
+
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Task.WaitAll()
+        }
+    }
+}
---
+++ ParkingApplication/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingApplication
+{
+    class Program
+    {
+
+        static void Welcome()
+        {
+            Console.WriteLine("\t\t\tWelcome to \"Rent Car\" application" + "\n" +
+                "\t\t\t________________________\n" +
+                "by TMS student Alexander Borisyonok");
+
+            BudgetInput();
+        }
+
+        static void BudgetInput()
+        {
+            double budget;
+
+            Console.WriteLine("Enter your budget in dollars: ");
+            do
+            {
+                while (!double.TryParse(Console.ReadLine(), out budget))
+                {
+                    Console.WriteLine("Wrong input. Try again..");
+                }
+            }
+            while (budget < 0);
+
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("What do you want? \n" +
+                "A) Rent a car\n" +
+                "B) Return a car" +
+                "C) Check for the cars in the parking lot");
+
+            char usersChoice = Console.ReadKey().KeyChar;
+            char.ToLower(usersChoice);
+
+            switch (usersChoice)
+            {
+                case 'a':
+
+                    break;
+                case 'b':
+
+                    break;
+                case 'c':
+
+                    break;
+            }
+        }
+
+        static void CarList()
+        {
+            Car car = new Car("Audi", 2000, 2000);
+            Car.AddToList(car);
+            Car car1 = new Car("BMW", 2005, 5000);
+            Car.AddToList(car1);
+            Car car2 = new Car("Volkswagen", 2010, 10000);
+            Car.AddToList(car2);
+
+
+            Console.WriteLine("Do you want to rent a car? y/n");
+            if (Console.ReadKey().KeyChar == 'y')
+            {
+                Console.Clear();
+                Car.RentImplementation();
+            }
+            else
+            {
+
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            CarList();
+
+
+            Console.ReadLine();
+        }
+    }
+}
